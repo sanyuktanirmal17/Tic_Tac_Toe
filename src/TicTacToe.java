efe8b1dc21e99bd7affb670d9431600a7c3bcd22
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-04 23:22:22 +0530

    Added: choose slot

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class TicTacToe {
 
-	public static char [] board = new char [10];
-	
+//	private static final String Char = null;
+	public static char[] board = new char [10];
+	static String turn;
 	public static void createBoard() {
 		for(int i=1; i< board.length; i++)
 			board[i]= ' ';
@@ -35,17 +36,42 @@ public class TicTacToe {
         System.out.println("|---|---|---|");
     }
 	
+
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
+        turn = "X";
+		String winner = null;
+		
+		for(int i =0; i<board.length; i++)
+		{
+//			board[i]= char.valueOf(i+1);
+		}
 		int player;
 		createBoard();
 		System.out.println("game created");
 		choosePlay(0);
 		System.out.println("Enter character 'X' and 'O'");
 		showBoard();
+		
+		while (winner == null) {
+            int numInput;
+            try {
+                numInput =sc.nextInt();
+                if (!(numInput > 0 && numInput <= 9)) {
+                    System.out.println(
+                        "Invalid input; re-enter slot number:");
+                    continue;
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println(
+                    "Invalid input; re-enter slot number:");
+                continue;
+            }
 		}
 	 }
+}
 	
 
 	
