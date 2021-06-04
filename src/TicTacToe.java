d614867b4fd6aa97334c085b981dea555e644c91
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-04 22:26:13 +0530

    Added: board created

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -18,6 +18,23 @@ public class TicTacToe {
 		    }
 		}
 	
+	static void showBoard()
+    {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                           + board[1] + " | " + board[2]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                           + board[4] + " | " + board[5]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                           + board[7] + " | " + board[8]
+                           + " |");
+        System.out.println("|---|---|---|");
+    }
+	
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
@@ -26,6 +43,7 @@ public class TicTacToe {
 		System.out.println("game created");
 		choosePlay(0);
 		System.out.println("Enter character 'X' and 'O'");
+		showBoard();
 		}
 	 }
 	
