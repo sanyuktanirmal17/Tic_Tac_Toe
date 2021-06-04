3803cbf8e2a17703c8291c7627beb7c3fc1944aa
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-04 10:08:57 +0530

    Added : Choose player

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,20 +2,32 @@ import java.util.*;
 public class TicTacToe {
 
 	public static char [] board = new char [10];
-	public static void main(String args[])
-	{
-		createBoard();
-		
-	}
+	
 	public static void createBoard() {
 		for(int i=1; i< board.length; i++)
 			board[i]= ' ';
 	}
-
-
-
-
-
-    }
+	
+	public static void choosePlay(int player)
+	{
+		char character;
+		if (player == 1) {
+		      character = 'X';
+		    } else {
+		      character = 'O';
+		    }
+		}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int player;
+		createBoard();
+		System.out.println("game created");
+		choosePlay(0);
+		System.out.println("Enter character 'X' and 'O'");
+		}
+	 }
+	
 
 	
