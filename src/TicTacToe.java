4d38668020999304e2daf967ece605c2711b9181
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-04 09:28:16 +0530

    Added : Game Board

---
+++ src/TicTacToe.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class TicTacToe {
+
+	public static char [] board = new char [10];
+	public static void main(String args[])
+	{
+		createBoard();
+		
+	}
+	public static void createBoard() {
+		for(int i=1; i< board.length; i++)
+			board[i]= ' ';
+	}
+
+
+
+
+
+    }
+
+	
